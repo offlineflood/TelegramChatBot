3048ab9b6559a13d57196a4a1553fad538ed9918
Aliyev Ali <108521240+offlineflood@users.noreply.github.com>
2025-04-17 01:41:01 +0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+config = {
+    "OWNER_ID": 5570119623,
+    "BOTS": [
+        {
+            "SESSION_NAME": "OneTelegramBot",
+            "API_ID": 16157584,
+            "API_HASH": "2167d4e6007a79eed91d084bf5b8966a",
+            "BOT_TOKEN": "7199899106:AAFGGdv70q-az4ACdm8CHtIuX-b99b7AERQ",
+            "ONE_BOT_ID": 7199899106,
+        },
+        {
+            "SESSION_NAME": "TwoTelegramBot",
+            "API_ID": 16157584,
+            "API_HASH": "2167d4e6007a79eed91d084bf5b8966a",
+            "BOT_TOKEN": "7926084993:AAEtyhjlAvz1kOgVkarT4-GggzNblk1EBmE",
+            "TWO_BOT_ID": 7926084993,
+        },
+        {
+            "SESSION_NAME": "ThreeTelegramBot",
+            "API_ID": 16157584,
+            "API_HASH": "2167d4e6007a79eed91d084bf5b8966a",
+            "BOT_TOKEN": "7319055038:AAHcR1SBezoSCfOA94TP5MooDLl6ahE4XRU",
+            "THREE_BOT_ID": 7319055038,
+        }
+    ]
+}
